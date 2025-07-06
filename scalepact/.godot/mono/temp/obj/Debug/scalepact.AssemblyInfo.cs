e152e8f0d39831307136f99e0a9f440d775724a0
Rin <alexisbarta@outlook.com>
2025-07-06 14:21:42 -0700

    Added in various addons, most won't be used until later
    Includes: Dialogue Manager 3, Spatial Gardener, GUIDE, SceneBuilder, Terrabrush, unidot importer

--- scalepact/.godot/mono/temp/obj/Debug/scalepact.AssemblyInfo.cs
+++ scalepact/.godot/mono/temp/obj/Debug/scalepact.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("scalepact")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f805561323eef201248fd245e4bf0d2d140b6775")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db55282aab06e10b1e26330876002186d978692e")]
 [assembly: System.Reflection.AssemblyProductAttribute("scalepact")]
 [assembly: System.Reflection.AssemblyTitleAttribute("scalepact")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
